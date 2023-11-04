43ba4bcbe0b000b19c60aefaff02a969d0f505a7
Tix3Dev <yves.vollmeier.code@gmail.com>
2023-11-04 12:54:53 +0100

    Add license headers

--- server-side/main.py
+++ server-side/main.py
@@ -1,3 +1,18 @@
+# This file is part of a quantum circuit simulator
+# Everything is openly developed on GitHub: https://github.com/Tix3Dev/swiqcs
+#
+# Copyright (C) 2023  Yves Vollmeier <https://github.com/Tix3Dev> and main collaborators
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from qcomputation import *
